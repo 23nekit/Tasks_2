acd69b46c256b4937811c49528516a6e860d6424
23nekit <23nekit@gmail.com>
2020-03-25 18:48:47 +0200

    Build 1.0.1 build version 2
    Build the last task:
    -Standart Event and Static Event

---
+++ Assets/Events/Scripts/StandartEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandartEvents : MonoBehaviour
+{
+    public event Action StandartEvent = delegate { };
+    public void PressButton()
+    {
+        StandartEvent += WriteToConsoleHello;//подписаться на событие
+        StandartEvent.Invoke();
+        StandartEvent -= WriteToConsoleHello;//отписаться от события
+    }
+    public void PressButton2()
+    {
+        StandartEvent += WriteToConsoleHello;
+        StandartEvent.Invoke();
+        StandartEvent += WriteToConsoleHello;
+        StandartEvent.Invoke();
+    }
+    void WriteToConsoleHello()
+    {
+        Debug.Log("Hello");
+    }
+}
---
+++ Assets/Events/Scripts/StaticEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEvents : MonoBehaviour
+{
+    public static event Action StaticEvent = delegate { };
+    public void PressButton()
+    {
+        StaticEvent += WriteToConsoleHi;//подписаться на событие
+        StaticEvent.Invoke();
+        StaticEvent -= WriteToConsoleHi;//отписаться от события
+    }
+    public void PressButton2()
+    {
+        StaticEvent += WriteToConsoleHi;
+        StaticEvent.Invoke();
+        StaticEvent += WriteToConsoleHi;
+        StaticEvent.Invoke();
+    }
+    void WriteToConsoleHi()
+    {
+        Debug.Log("Hi");
+    }
+}
